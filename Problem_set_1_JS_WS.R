3b4d0e564ae55afb99f764c5840a7ec0c629557e
wsanch92 <w.sanchezs@uniandes.edu.co>
2022-06-11 10:33:25 -0500

    eliminé script

--- Problem_set_1_JS_WS.R
+++
@@ -1,4 +0,0 @@
-#Autores: Juan Sebastián Vásquez Acevedo y Walter Leonardo Sánchez
-#Curso: Big Data and Machine Learning
-#PROBLEM SET 1
-#
--- Scrips/Problem_set_1_JS_WS.R
+++ Scrips/Problem_set_1_JS_WS.R
@@ -1,4 +1,33 @@
 #Autores: Juan Sebastián Vásquez Acevedo y Walter Leonardo Sánchez
 #Curso: Big Data and Machine Learning
 #PROBLEM SET 1
-#
+#Creación: 11/06/2022
+
+
+
+## Instalar packages
+
+##llamar packages
+
+
+## Cargar datos
+
+
+
+## Creación y modificación de variables
+
+
+
+## Tablas y summarice
+
+
+
+## Gráficos
+
+
+
+
+## Regresiones y resultados
+
+
+
