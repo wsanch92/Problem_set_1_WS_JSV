73d29fbe9988e07448abca51679c4e7bf5fbfb9c
wsanch92 <w.sanchezs@uniandes.edu.co>
2022-06-18 00:36:38 -0500

    Crear For para Scraping

--- Scrips/Problem_set_1_JS_WS.R
+++ Scrips/Problem_set_1_JS_WS.R
@@ -6,42 +6,62 @@
 
 
 ## Instalar packages
+#install.packages("XML")
 
 ##llamar packages
 require(pacman)
-p_load(tidyverse, rvest)
+p_load(tidyverse, rvest, tibble)
 
 
 ## Scraping de los datos en chunk
-url_geih18<-"https://ignaciomsarmiento.github.io/GEIH2018_sample/"
+url_geih18 <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/"
 browseURL(url_geih18)
 
 ## leer el html de la página del profe
-html_t1 = read_html(url_geih18) 
+html_t1 <- read_html(url_geih18) 
 class(html_t1)
 
 ## topar el Xpath para encontar las url de los chuns
 html_t1 %>% html_nodes(xpath = 'html/body/div/div/div[2]/ul/li[1]/a')
 
 ## Extraer los elementos del nodo a correspndientes a los links
-elements = html_t1 %>% html_elements("a")
+elements <- html_t1 %>% html_elements("a")
 elements
 
 #Almacenar los links en un objeto
-refs = elements %>% html_attr("href")
-refs = refs[6:15]
+refs <- elements %>% html_attr("href")
+refs <- refs[6:15]
 
-titles = elements %>% html_text()
-titles = titles[6:15]
-dc1<-tibble(titles,url=paste0(url_geih18,refs))
-titles
+#Referenciar los nombres de los chunk y enlistar las url obtenidad para cada chunk en un tibble
+nom_chunk <- elements %>% html_text()
+nom_chunk <- nom_chunk[6:15]
+db_url<-tibble(nom_chunk,url=paste0(url_geih18,refs))
 
-dc1 %>% head()
+## Revisar el tibble de urls
+db_url %>% head()
+db_url$url[1]
 
-browseURL(dc1$url[1])
+# Rango de filas que corresponde al número de chunks
+n <- nrow(db_url)
+n
+#Se crea un Data Frame vacio para agregar todas las bases obtenidas 
+db <- data.frame()
+
+#Elaboración de loop para agregar todas las particiones de la GEIH
+for (i in 1:n){
+  urls <- db_url$url[i] %>% read_html() %>% html_elements("div") %>% html_attr("w3-include-html") #se obtine la parte de url verdadera
+  urls <- urls[8] #Exploración del elemento 8 donde esta la url verdadera
+  dc<-tibble(nom_chunk,urls=paste0(url_geih18,urls)) #unir la url principal con la parte de url verdadera
+  dbc <- dc$urls[i] %>% read_html() %>% html_nodes("table") %>% html_table() %>% as.data.frame() # extracción de la tabla y conversión en DataFrame
+  db <- rbind(db, dbc) # append de las bases
+  cat("chunk OK:", i, ", ")
+}
+
+db <- as_tibble(db)
+
+
 
 ## Cargar datos
-
 
 
 ## Creación y modificación de variables
