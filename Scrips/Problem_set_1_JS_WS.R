a6d6ece25d0481b322b3d41b1af42a1b8c090a07
Juan Sebastian Vasquez Acevedo <js.vasquez@uniandes.edu.co>
2022-06-18 21:34:16 -0500

    Inicio de la limpieza de la base de datos

--- Scrips/Problem_set_1_JS_WS.R
+++ Scrips/Problem_set_1_JS_WS.R
@@ -10,27 +10,29 @@
 
 ##llamar packages
 require(pacman)
-p_load(tidyverse, rvest, tibble)
+p_load(tidyverse, rvest, tibble,rio,skimr,caret)
+
 
 
 ## Scraping de los datos en chunk
 url_geih18 <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/"
 browseURL(url_geih18)
 
-## leer el html de la página del profe
+## leer el html de la página del problem set
 html_t1 <- read_html(url_geih18) 
 class(html_t1)
 
-## topar el Xpath para encontar las url de los chuns
+## tomar el Xpath para encontar las url de los chuns
 html_t1 %>% html_nodes(xpath = 'html/body/div/div/div[2]/ul/li[1]/a')
 
 ## Extraer los elementos del nodo a correspndientes a los links
 elements <- html_t1 %>% html_elements("a")
 elements
 
-#Almacenar los links en un objeto
+#Almacenar los links que necesitamos en un objeto
 refs <- elements %>% html_attr("href")
 refs <- refs[6:15]
+refs
 
 #Referenciar los nombres de los chunk y enlistar las url obtenidad para cada chunk en un tibble
 nom_chunk <- elements %>% html_text()
@@ -59,18 +61,24 @@ for (i in 1:n){
 
 db <- as_tibble(db)
 
-
+saveRDS(db,file="Poblem_set_1_WS_JSV/Data/GEIH_2018.rds")
 
 ## Cargar datos
 
 
+
 ## Creación y modificación de variables
 
+## Base con personas mayores de 18 años y que esten ocupados
 
+db_1 <- db_1 %>% mutate(db_1,)
+db_1 <- db_1[db_1$age >= 18 & db_1$ocu == 1, ]
 
 ## Tablas y summarice
 
+## Resumen de toda la base
 
+skim(db)
 
 ## Gráficos
 
