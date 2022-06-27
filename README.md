36cfae205ac27b31e9291811dcef2ea037a6dd2c
wsanch92 <w.sanchezs@uniandes.edu.co>
2022-06-27 13:17:45 -0500

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Problem_set_1_WS_JSV
  
+#Autores: Juan Sebastián Vásquez Acevedo y Walter Leonardo Sánchez
+#Curso: Big Data and Machine Learning
+#PROBLEM SET 1
+#Creación: 11/06/2022
+
+# Paquetes necesarios:
+	#pacman: este paquete contiene una funcionalidad llamada 'p_load', que permite llamar las librerías que requerimos, sino, intenta instalarlas en la medida de lo posible.
+	#tidyverse
+	#rvest
+	#tibble
+	#skimr
+	#caret
+	#stargazer
+	#boot
+
+# Objetivos: 
+		1) Extraer de la web una base de datos de la Gran Encuesta Integrada de Hogares (GEIH)- 2018, utilizando técnicas de Web Scraping.
+		2) Construir y estimar el mejor modelo de predicción de los ingresos controlando por variables que ayuden esclarecer el comportamiento por género, en las personas ocupadas mayores a 18 años
+
+# El Script:
+	Se contruye un script en R que permite llevar a cabo los diferentes objetivos utilizando regresiones, tablas y gráficos.
+
+	En primera instancia, el script desarrolla la técnica de Web Scraping con funciones del paquete "rvest" extrayendo, a través de un proceso iterativo, las particiones de las bases
+de la GEIH-2018 alojadas en la página web "https://ignaciomsarmiento.github.io/GEIH2018_sample/", y finalmente, dejar una sola base para uso del proyecto llamada "db". 
+Esta base, sufre algunas adecuaciones como creación de varibales o filtros para logra un Data Frame final: db_final.
+
+A partir de la base se estiman modelos de ingresos controlando por variables que a la luz de la literatura, tienen un efecto causal en la tenencia de ingresos. 
--- Scrips/Problem_set_1_JS_WS.R
+++ Scrips/Problem_set_1_JS_WS.R
@@ -329,8 +329,8 @@ prueba_boot.fn<-function(db_mujer,index){
   coef(lm(log_ingreso~edad+edad_sqr+niveleduc+tiempoempresa+tiempoempresa_sqr,data=db_mujer, subset = index))
 }
 
-boot<-boot(db_mujer, prueba_boot.fn, R=1000)
-boot
+boot_m<-boot(db_mujer, prueba_boot.fn, R=1000)
+boot_m
 
 mod_m
 stargazer(mod_m, type="text")
@@ -345,8 +345,8 @@ prueba_boot.fn<-function(db_hombre,index){
   coef(lm(log_ingreso~edad+edad_sqr+niveleduc+tiempoempresa+tiempoempresa_sqr,data=db_hombre, subset = index))
 }
 
-boot<-boot(db_hombre, prueba_boot.fn, R=1000)
-boot
+boot_h<-boot(db_hombre, prueba_boot.fn, R=1000)
+boot_h
 
 mod_h
 stargazer(mod_h, type="text")
@@ -389,8 +389,8 @@ mse_mod2
 
 ###################################################################
 ## DATOS ATÍPICOS
-Q<- quantile(db_final$log_ingreso,probs = c(0.25,0.75),na.rm=F)
-iqr <- IQR(x=db_final$log_ingreso , na.rm=F)
+Q<- quantile(db_final$ingreso,probs = c(0.25,0.75),na.rm=F)
+iqr <- IQR(x=db_final$ingreso , na.rm=F)
 up <- Q[2]+1.5*iqr # Upper Range
 low<- Q[1]-1.5*iqr # Lower Range
 ###################################################################
@@ -438,7 +438,7 @@ y_real_fuera_sex <- prueba$ingreso
 mse_dentro_sex <- mean((y_real_dentro_sex-y_hat_dentro_sex)^2)
 mse_fuera_sex <- mean((y_real_fuera_sex-y_hat_fuera_sex)^2)
 
-mse_fuera_sex/mse_dentro_sex ### ver el overfitting
+mse_fuera_sex/mse_dentro_sex ### 
 
 ## Modelo por sexo y edad
 
@@ -592,9 +592,9 @@ for (j_h in 1:N_t){
                                               1,0),
                             res_e_x=lm(e_j~edad+edad_sqr+niveleduc+tiempoempresa+tiempoempresa_sqr,test)$residuals,
   )
-  reg4<-lm(res_y_x~res_e_x,test)
+  reg_laverage<-lm(ingreso~edad+edad_sqr+niveleduc+tiempoempresa+tiempoempresa_sqr,data=test)
   u_j<-lm(ingreso~edad+edad_sqr+niveleduc+tiempoempresa+tiempoempresa_sqr,data=test)$residual[j_h]
-  h_j<-lm.influence(reg1)$hat[j_h]
+  h_j<-lm.influence(reg_laverage)$hat[j_h]
   alpha<-u_j/(1-h_j)
   laverages <- c(laverages,alpha)
 }
@@ -602,13 +602,12 @@ for (j_h in 1:N_t){
 
 length(laverages) ## comprobar el largo de laverage para ver si tiene el mismo número de filas que la muestra de copia test
 prueba$laverage <- laverages ## pegar al dataframe de prueba los laverages
-prueba <- prueba %>% mutate(atipico = ifelse((log_ingreso>up | log_ingreso< low),
-                                             1,
-                                             0)) ## crear variable que marca si es atípico o no
 ## Ordenar la muestra de prueba para ver si los laverages mayores tienen datos atípicos
 prueba <- prueba[order(prueba$laverage,decreasing = T),]
 
 
+ggplot(prueba, aes(x=laverage, y=ingreso)) +
+  geom_line()
 
 
 #### Punto 5. b)
@@ -641,10 +640,6 @@ for (i in modelos){
 MSE_CV
 MSE_CV_2<-c(log(MSE_CV[6]))
 MSE_CV_2 #### Las unidades de medida están generando problemática
-
-
-
-
 
 
 
